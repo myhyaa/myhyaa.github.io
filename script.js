8eeeb8d6f78199039e41187d42659c1ac5574e7e
myhyaa <64518421+myhyaa@users.noreply.github.com>
2020-05-14 23:51:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const calculatorScreen=document.querySelector(".calculator-screen");
+const calculatorScreen=document.querySelector('.calculator-screen')
 const updateScreen=(number)=>{
-  calculatorScreen.value=number;
-};
-const numbers=document.querySelectorAll(".number");
+  calculatorScreen.value=number
+}
+const numbers=document.querySelectorAll(".number")
 numbers.forEach((number)=>{
   number.addEventListener("click",(event)=>{
-  updateScreen(event.target.value);
-  });
-});
+  updateScreen(event.target.value)
+  })
+})
 
   
