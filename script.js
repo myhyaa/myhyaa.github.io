ecfbc633e321754d8b63f8e99dac37179f7ce90e
myhyaa <64518421+myhyaa@users.noreply.github.com>
2020-05-15 00:07:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let prevNumber=''
+let calculationOperator=''
+let currentNumber='0'
+const inputNumber=(number)=>{
+  if(currentNumber==='0'){
+    currentNumber=number
+  }else{
+  currentNumber+=number
+  }
+}
 const calculatorScreen=document.querySelector('.calculator-screen')
 const updateScreen=(number)=>{
   calculatorScreen.value=number
@@ -5,8 +15,19 @@ const updateScreen=(number)=>{
 const numbers=document.querySelectorAll(".number")
 numbers.forEach((number)=>{
   number.addEventListener("click",(event)=>{
-  updateScreen(event.target.value)
+    inputNumber(event.target.value)
+    updateScreen(currentNumber)
+  })
+})
+const inputOperator=(operator)=>{
+  prevNumber=currentNumber
+  calculationOperator=operator
+  currentNumber=''
+}
+const operators=document.querySelectorAll(".operator")
+operators.forEach((operator)=>{
+  operator.addEventListener("click",(event)=>{
+    inputOperator(event.target.value)
   })
 })
 
-  
